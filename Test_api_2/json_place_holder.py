d0b2272456cc69fc0953fabeaacb332b8f806741
Aleksandr Kryachko <Aleksan.Kryachko@gmail.com>
2023-02-28 18:20:32 +0700

    jsonschema

--- Test_api_2/json_place_holder.py
+++ Test_api_2/json_place_holder.py
@@ -1,6 +1,7 @@
 import  requests
 import json
 import jsonschema
+from jsonschema import Draft7Validator
 # https://realpython.com/testing-third-party-apis-with-mocks/
 # https://opensource.com/article/21/9/unit-test-python
 
@@ -9,21 +10,21 @@ r = requests.get(url=url)
 # assert r.status_code == 200, 'Ответа нет'
 answer = r.json()
 answer_json = answer[0]
-с = r.status_code
+
 assert(r.status_code == 200), 'статус не 200'
-if r.status_code != 200:
-    print('статус не 200')
 
 assert answer_json == {'userId': 1, 'id': 1, 'title': 'delectus aut autem', 'completed': False}, 'нет информации для вывода'
 # url = 'https://jsonplaceholder.typicode.com/posts/1'
 # r = requests.get(url=url)
-print(answer_json)
+
+json_data = r.json()
 schema = {
-   "userId": "integer",
-   "id": "integer",
+"items":{
+   "userId": "number",
+   "id": "number",
    "title": "string",
-    "completed": "string",
-    }
-{
-  "required": ["userId", "id", "title", "completed"]
-}
+    "completed": "False"},
+          }
+required: ["userId", "id", "title", "completed"]
+assert Draft7Validator(schema).is_valid(json_data), "Ошибка Валидации"
+print(answer_json)
