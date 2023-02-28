e77de3ea8260d85cde990f48778049abdff7b3e0
Aleksandr Kryachko <Aleksan.Kryachko@gmail.com>
2023-02-28 11:09:14 +0700

    json

--- Test_api_2/json_place_holder.py
+++ Test_api_2/json_place_holder.py
@@ -1,1 +1,15 @@
+import  requests
+import json
+# https://realpython.com/testing-third-party-apis-with-mocks/
+# https://opensource.com/article/21/9/unit-test-python
+
 url = 'http://jsonplaceholder.typicode.com/todos'
+r = requests.get(url=url)
+# assert r.status_code == 200, 'Ответа нет'
+answer = r.json()
+answer_json = answer[0]
+if r.status_code != 200:
+    print('статус не 200')
+# if заменить на asert
+print(answer_json)
+
---
+++ Test_api_2/xml_parser.py
@@ -0,0 +1,10 @@
+# https://docs.python.org/3/library/xml.etree.elementtree.html
+url = 'https://www.washingtonpost.com/arcio/news-sitemap/'
+'https://www.google.com/schemas/sitemap-news/0.9/sitemap-news.xsd'
+
+
+
+# sql test
+'https://pypi.org/project/sql-test/'
+# https://learn.microsoft.com/en-us/sql/connect/python/python-driver-for-sql-server?view=sql-server-ver16
+# https://stackoverflow.com/questions/11451101/retrieving-data-from-sql-using-pyodbc
