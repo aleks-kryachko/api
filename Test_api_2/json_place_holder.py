5c25ca80ab55a564377934aa3444cd35bd346b8f
Aleksandr Kryachko <Aleksan.Kryachko@gmail.com>
2023-02-28 15:11:16 +0700

    jsonschema

--- Test_api_2/json_place_holder.py
+++ Test_api_2/json_place_holder.py
@@ -1,5 +1,6 @@
 import  requests
 import json
+import jsonschema
 # https://realpython.com/testing-third-party-apis-with-mocks/
 # https://opensource.com/article/21/9/unit-test-python
 
@@ -8,8 +9,21 @@ r = requests.get(url=url)
 # assert r.status_code == 200, 'Ответа нет'
 answer = r.json()
 answer_json = answer[0]
+с = r.status_code
+assert(r.status_code == 200), 'статус не 200'
 if r.status_code != 200:
     print('статус не 200')
-# if заменить на asert
-print(answer_json)
 
+assert answer_json == {'userId': 1, 'id': 1, 'title': 'delectus aut autem', 'completed': False}, 'нет информации для вывода'
+# url = 'https://jsonplaceholder.typicode.com/posts/1'
+# r = requests.get(url=url)
+print(answer_json)
+schema = {
+   "userId": "integer",
+   "id": "integer",
+   "title": "string",
+    "completed": "string",
+    }
+{
+  "required": ["userId", "id", "title", "completed"]
+}
