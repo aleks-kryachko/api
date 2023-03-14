cabbc6922516ec1c190b04d21d51c0e8db15c8d1
aleksandr kryachko <aleksan.kryachko@gmail.com>
2023-03-14 21:50:26 +0700

    locust

--- locustfile.py
+++ locustfile.py
@@ -7,46 +7,46 @@ from locust import HttpUser, SequentialTaskSet, task, between
 class User(HttpUser):
     @task
     class SequenceOfTasks(SequentialTaskSet):
-        wait_time = between(1, 5)
+        wait_time = between(1, 3)
 
         @task
         def mainPage(self):
-            self.client.get("/")
-            self.client.get("https://api.demoblaze.com/entries")
+            # self.client.get("/")
+            self.client.get("http://jsonplaceholder.typicode.com/todos")
 
         @task
         def login(self):
-            self.client.options("https://api.demoblaze.com/login")
-            self.client.post("https://api.demoblaze.com/login", json={"username": "aaaa", "password": "YWFhYQ=="})
-            self.client.options("https://api.demoblaze.com/check")
-            self.client.get("https://api.demoblaze.com/entries")
-            self.client.post("https://api.demoblaze.com/check", json={"token": "YWFhYTE2MzA5NDU="})
+            # self.client.options("https://api.demoblaze.com/login")
+            # self.client.post("https://api.demoblaze.com/login", json={"username": "aaaa", "password": "YWFhYQ=="})
+            # self.client.options("https://api.demoblaze.com/check")
+            self.client.get("http://jsonplaceholder.typicode.com/todos/1")
+            # self.client.post("https://petstore.swagger.io/v2/swagger.json", json={"token": "YWFhYTE2MzA5NDU="})
 
         @task
         def clickProduct(self):
-            self.client.get("/prod.html?idp_=1")
-            self.client.options("https://api.demoblaze.com/check")
-            self.client.options("https://api.demoblaze.com/view")
-            self.client.post("https://api.demoblaze.com/check", json={"token": "YWFhYTE2MzA5NDU="})
-            self.client.post("https://api.demoblaze.com/view", json={"id": "1"})
+            self.client.get("http://jsonplaceholder.typicode.com/todos/2")
+            # self.client.options("https://api.demoblaze.com/check")
+            # self.client.options("https://api.demoblaze.com/view")
+            # self.client.post("https://api.demoblaze.com/check", json={"token": "YWFhYTE2MzA5NDU="})
+            # self.client.post("https://api.demoblaze.com/view", json={"id": "1"})
 
-        @task
-        def addToCart(self):
-            self.client.options("https://api.demoblaze.com/addtocart")
-            self.client.post("https://api.demoblaze.com/addtocart",
-                             json={"id": "fb3d5d23-f88c-80d9-a8de-32f1b6034bfd", "cookie": "YWFhYTE2MzA5NDU=",
-                                   "prod_id": 1, "flag": 'true'})
+        # @task
+        # def addToCart(self):
+            # self.client.options("https://api.demoblaze.com/addtocart")
+            # self.client.post("https://api.demoblaze.com/addtocart",
+            #                  json={"id": "fb3d5d23-f88c-80d9-a8de-32f1b6034bfd", "cookie": "YWFhYTE2MzA5NDU=",
+            #                        "prod_id": 1, "flag": 'true'})
 
-        @task
-        def viewCart(self):
-            self.client.get("/cart.html")
-            self.client.options("https://api.demoblaze.com/check")
-            self.client.options("https://api.demoblaze.com/viewcart")
-            self.client.post("https://api.demoblaze.com/check", json={"token": "YWFhYTE2MzA5NDU="})
-            self.client.post("https://api.demoblaze.com/viewcart", json={"cookie": "YWFhYTE2MzA5NDU=", "flag": 'true'})
-            self.client.options("https://api.demoblaze.com/view")
-            self.client.post("https://api.demoblaze.com/check", json={"token": "YWFhYTE2MzA5NDU="})
-            self.client.post("https://api.demoblaze.com/view", json={"id": "1"})
+        # @task
+        # def viewCart(self):
+            # self.client.get("/cart.html")
+            # self.client.options("https://api.demoblaze.com/check")
+            # self.client.options("https://api.demoblaze.com/viewcart")
+            # self.client.post("https://api.demoblaze.com/check", json={"token": "YWFhYTE2MzA5NDU="})
+            # self.client.post("https://api.demoblaze.com/viewcart", json={"cookie": "YWFhYTE2MzA5NDU=", "flag": 'true'})
+            # self.client.options("https://api.demoblaze.com/view")
+            # self.client.post("https://api.demoblaze.com/check", json={"token": "YWFhYTE2MzA5NDU="})
+            # self.client.post("https://api.demoblaze.com/view", json={"id": "1"})
 
 # class AwesomeUser(HttpUser):
 #     tasks = ['CredentialLoadTest']
